0b5b2b10642b34096ddf7b48faef43c740437e32
Adora González <Adora_Gonzalez@Jabil.com>
2025-03-14 14:01:19 -0600

    Advance button styles

--- src/app/layout/style-2025/button/button.component.ts
+++ src/app/layout/style-2025/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ContentChild, ElementRef, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./button.component.css']
 })
 export class ButtonComponent {
-
+  @Input() icon?: string;
+  @Input() iconPos: 'left' | 'right' = 'left';
+  @Input() classList: string = ''; 
+  @Input() type: 'button' | 'reset' | 'submit' = 'button';
+  @Input() name?: string;
+  @Input() id?: string;
+  @Output() onClick = new EventEmitter<Event>();
+  @ContentChild('content', { static: false }) content!: ElementRef;
+  hasContent = false;
+  ngAfterContentInit() {
+    this.hasContent = this.content?.nativeElement?.textContent.trim().length > 0;
+  }
 }
